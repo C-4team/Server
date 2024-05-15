6ad0ae134df1cf0445af4e1ffea2d95248df2c53
honeybears <clone0922@gmail.com>
2024-05-16 00:18:28 +0900

    init commit

---
+++ repositorys/Repository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team4.repository
+{
+    internal interface Repository<I,T>
+    {
+        T Get(int id);
+        void Insert(T item);
+        void Update(I id, T item);
+        void Delete(I id);
+    }
+}
