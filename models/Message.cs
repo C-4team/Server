2748c7d81201a53eeda63f3129fc90c782179bd3
honeybears <clone0922@gmail.com>
2024-05-16 02:16:59 +0900

    Message Model 구현

---
+++ models/Message.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team4.models
+{
+    public class Message
+    {
+        private long roomid;
+
+        private long msgid;
+        
+        private long userid;
+
+        private string message;
+
+        public Message(long roomid, long msgid, long userid, string message) 
+        {
+            this.roomid = roomid;
+            this.msgid = msgid;
+            this.userid = userid;
+            this.message = message;
+        }
+
+        public long getId() 
+        {
+            return roomid;
+        }
+        public long getRoomid()
+        {
+            return roomid;
+        }
+        public long getMsgid()
+        {
+            return msgid;
+        }
+        public long getUserid()
+        {
+            return userid;
+        }
+        
+
+    }
+}
---
+++ services/ChatService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team4.services
+{
+    public class ChatService
+    {
+        private UserRepository userRepository;
+        public ChatService(UserRepository userRepository) 
+        {
+            this.userRepository = userRepository;
+        }
+    }
+}
